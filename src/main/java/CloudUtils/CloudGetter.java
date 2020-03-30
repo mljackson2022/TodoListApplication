ca16c58382635b2bb834227d00346ce2511d64e1
Dot Horsman <dottyhorsman@bsu.edu>
2020-03-30 12:22:17 -0400

    added correct info to README.md

--- src/main/java/CloudUtils/CloudGetter.java
+++ src/main/java/CloudUtils/CloudGetter.java
@@ -1,4 +1,18 @@
 package CloudUtils;
 
-public class CloudGetter {
+import com.google.api.client.http.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CloudGetter
+{
+    HttpRequestFactory requestFactory;
+    String baseURL = "https://todoserver222.herokuapp.com/";
+    String todosURL = baseURL + "todos/";
+
+
 }
