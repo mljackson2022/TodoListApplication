eccef0b5e1ffa2cdecd47f4867fa6804588ec7a7
Jiahao Fu <jfu@bsu.edu>
2020-04-17 20:54:15 -0400

    Add comments, rename some methods

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -45,13 +45,13 @@ public class UI extends JFrame implements ActionListener {
         panel.setLayout(gridBagLayout);
         setContentPane(panel);
 
-        //Area showing todoitems
+        //Area showing to-do items
         todoItems = new JTextArea("This will show all current todo items");
         var recentConstraints = new GridBagConstraints(1, 0, 1,5 , 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(10, 10, 10, 10), 0, 0);
         panel.add(todoItems, recentConstraints);
         todoItems.setSize(900,900);
 
-        //Operation tips Label
+        //Operation tips label
         titleNote = new JLabel("Please enter a title:");
         var termConstraints = new GridBagConstraints(2, 0, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         panel.add(titleNote, termConstraints);
@@ -63,7 +63,7 @@ public class UI extends JFrame implements ActionListener {
         panel.add(title, titleConstraints);
         title.setSize(150,150);
 
-        //Operation tips Label
+        //Operation tips label
         DescriptionNote = new JLabel("Please enter a description:");
         var DescriptionConstraints = new GridBagConstraints(2, 2, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         panel.add(DescriptionNote, DescriptionConstraints);
@@ -75,7 +75,7 @@ public class UI extends JFrame implements ActionListener {
         panel.add(description, descriptionConstraints);
         description.setSize(150,150);
 
-        //Operation tips Label
+        //Operation tips label
         DueDateNote = new JLabel("Please enter a due date (Example:2020-04-15T12:00):");
         var DueDateConstraints = new GridBagConstraints(2, 4, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         panel.add(DueDateNote, DueDateConstraints);
@@ -87,7 +87,7 @@ public class UI extends JFrame implements ActionListener {
         panel.add(duedate, dateConstraints);
         duedate.setSize(150,150);
 
-        //Operation tips Label
+        //Operation tips label
         operateNote = new JLabel("Please enter the id of the item you want to operate:");
         var operateNoteConstraints = new GridBagConstraints(2, 6, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         panel.add(operateNote, operateNoteConstraints);
@@ -99,7 +99,7 @@ public class UI extends JFrame implements ActionListener {
         panel.add(operateID, ownerConstraints);
         operateID.setSize(150,150);
 
-        //Add buttton
+        //Add button
         add = new JButton("Add");
         var addConstraints = new GridBagConstraints(2, 10, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         add.addActionListener(new ActionListener() {
@@ -112,7 +112,7 @@ public class UI extends JFrame implements ActionListener {
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
-                //Add new todoItem
+                //Add new to-do item
                 String addItemTitle = title.getText();
                 String addItemDescription = description.getText();
                 String addItemDueDate = duedate.getText();
@@ -131,7 +131,7 @@ public class UI extends JFrame implements ActionListener {
             }
         });
 
-        //delete button
+        //Delete button
         delete = new JButton("Delete");
         var deleteConstraints = new GridBagConstraints(3, 10, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
         delete.addActionListener(new ActionListener() {
@@ -139,15 +139,15 @@ public class UI extends JFrame implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 //Get the id of the to-do item user wants to delete
                 int deleteItemID = Integer.parseInt(operateID.getText());
-                //delete item from todolist
+                //Delete item from todolist
                 list.deleteItem(deleteItemID);
-                //delete item from cloud
+                //Delete item from cloud
                 try {
                     cloud.deleteTodoItem(deleteItemID);
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
-                //Display current todoitems
+                //Display current to-do items
                 todoItems.setText(list.AllItemInformation());
             }
         });
@@ -158,6 +158,7 @@ public class UI extends JFrame implements ActionListener {
         sync.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Sync data from cloud to local
                 try {
                     String JsonString = cloud.getAllTeam4TodoItems();
                     list = parser.parseJsonTodoItem(JsonString);
@@ -182,9 +183,13 @@ public class UI extends JFrame implements ActionListener {
         snooze.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Get the id of the to-do item user wants to snooze
                 int snoozedItemID = Integer.parseInt(operateID.getText());
+                //Get the date of the to-do item user wants to snooze
                 String newDate = duedate.getText();
+                //Snooze item
                 list.snoozeItemDueDate(snoozedItemID, newDate);
+                //Display current to-do items
                 todoItems.setText(list.AllItemInformation());
             }
         });
@@ -197,9 +202,9 @@ public class UI extends JFrame implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 //Get the id of the to-do item user wants to complete
                 int completedItemID = Integer.parseInt(operateID.getText());
-                //complete item
+                //Complete item
                 list.completedItem(completedItemID);
-                //Display current todoitems
+                //Display current to-do items
                 todoItems.setText(list.AllItemInformation());
             }
         });
